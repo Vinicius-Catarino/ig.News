6482ed2436257b609d3a7b6e4cd0968c4ddf49bf
Vinicius Catarino <vinicius_catarino@outlook.com>
2022-06-29 18:07:21 -0300

    home page

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,29 @@
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+  const isUserLoggetIn = true;
+
+  return isUserLoggetIn ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+    >
+      <FaGithub color='#84d361' />
+      Vinicius Catarino
+      <FiX color='737380' className={styles.closeIcon} />
+    </button>
+  )
+    : (
+      <button
+        type="button"
+        className={styles.signInButton}
+      >
+        <FaGithub color='#eba417' />
+        Sign in with Github
+      </button>
+    )
+
+
+}
