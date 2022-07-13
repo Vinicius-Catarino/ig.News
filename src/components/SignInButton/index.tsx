0d42c533a160d0e39b290125457c507831e31077
Vinicius Catarino <vinicius_catarino@outlook.com>
2022-07-13 16:23:47 -0300

    login with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,20 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/react'
+
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggetIn = true;
-
-  return isUserLoggetIn ? (
+  const { data: session } = useSession();
+  console.log(session);
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color='#84d361' />
-      Vinicius Catarino
+      {session.user.name}
       <FiX color='737380' className={styles.closeIcon} />
     </button>
   )
@@ -19,6 +22,7 @@ export function SignInButton() {
       <button
         type="button"
         className={styles.signInButton}
+        onClick={() => signIn('github')}
       >
         <FaGithub color='#eba417' />
         Sign in with Github
